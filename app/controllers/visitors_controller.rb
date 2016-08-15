061f26394aa7c376b6c2b3dda93c7326f5cb515d
sovanlandy <sovanlandy@gmail.com>
2016-08-15 01:17:42 -0700

    ok

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,4 +3,8 @@ class VisitorsController < ApplicationController
 	def kickup
 		render :layout => false    
 	end
+
+	def support
+		
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
   match 'kickup', to: 'visitors#kickup', via: [:get]
+  match 'support', to: 'visitors#support', via: [:get]
 end
