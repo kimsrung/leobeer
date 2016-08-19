e858a9007a7de6582d2e9989c92890d677d598c7
sovanlandy <sovanlandy@gmail.com>
2016-08-19 02:14:57 -0700

    translate

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '1039085669520186', '51f90832c9a0adcb55c1211c83ef09c8'
+  provider :facebook, '1039085669520186', '51f90832c9a0adcb55c1211c83ef09c8', scope: "publish_actions,email,user_photos"
 end
