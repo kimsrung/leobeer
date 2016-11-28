ed23fbfcb6604c2e67fbc134b3a32deff7fb0463
kimsrung <kimsrung@yoolk.com>
2016-11-28 08:30:54 +0700

    remove unused files

--- config/routes.rb
+++ config/routes.rb
@@ -6,25 +6,12 @@ Rails.application.routes.draw do
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get]
   match 'auth/failure', to: redirect('/') ,via: [:get]
   match 'signout', to: 'sessions#destroy', as: 'signout', via: [:post]
-  
+
   resources :stamps do
-  	collection do 
+  	collection do
   		get 'download'
       post 'upload'
   	end
   end
-
-  resources :users, only: [:index] do 
-    collection do 
-      post 'score'
-    end
-  end
-
-  namespace :admin do 
-    resources :transactions do 
-
-    end
-  end
-  
 
 end
